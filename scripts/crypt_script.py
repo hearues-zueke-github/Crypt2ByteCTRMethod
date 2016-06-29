3063fc7ab19d954215165906132ae3e25b2dcff4
Haris Ziko <hziko314@gmail.com>
2016-06-29 14:58:06 +0200

    added one new script!

--- scripts/crypt_script.py
+++ scripts/crypt_script.py
@@ -1,13 +1,20 @@
 #! /usr/bin/python2.7
 
+from subprocess import call
 import os
 
-rootdir = "/home/haris/Documents"
+rootdir = "/home/haris/test_tree_folder"
+rootdir_input = ""
+rootdir_output = ""
 
 for root, subdirs, files in os.walk(rootdir):
     print("root = "+str(root))
     print("subdirs = "+str(subdirs))
     print("files = "+str(files))
-    for f in files:
-        print("size of "+str(f)+": "+str(os.stat(str(root+"/"+f)).st_size)+" bytes")
-    print("")
+    pass
+    # for f in files:
+    #     print("size of "+str(f)+": "+str(os.stat(str(root+"/"+f)).st_size)+" bytes")
+    # print("")
+
+call(["../binaries/crypt_2B_ctr_generic", "-c", "key_2B_e_10_r_2.byte", "10", "2"])
+call(["../binaries/crypt_2B_ctr_generic", "-c", "key_2B_e_10_r_2.byte", "10", "2"])
